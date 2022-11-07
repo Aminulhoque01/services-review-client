d6742fea997e3986093d7d7fce217c6d0b393cf9
Aminulhoque1 <aminulkupa50@gmail.com>
2022-11-07 11:04:01 -0800

    third commit

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -5,3 +5,53 @@ import app from '../../firebase/firebase.config';
 export const AuthContext = createContext();
 const auth = getAuth(app)
 
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loader, setLoader] = useState(true);
+
+    const createUser = (email,password)=>{
+        setLoader(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+    const loginUser = (email,password)=>{
+        
+        setLoader(true)
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+
+    const googleLogin =(provider)=>{
+        return signInWithPopup(auth,provider)
+    }
+    const logOut =()=>{
+        return signOut(auth)
+    }
+
+    useEffect(()=>{
+        const unSubscribe= onAuthStateChanged(auth, (currentUser)=>{
+            console.log(currentUser);
+            setUser(currentUser);
+            setLoader(false)
+        })
+        return ()=>{
+            unSubscribe();
+        }
+    },[])
+
+    const authInfo ={
+        user,
+        createUser,
+        loginUser,
+        loader,
+        googleLogin,
+        logOut
+    }
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
