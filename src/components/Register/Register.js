2d03f0da911ce1fdf62eeffe91d80c616c2cef1c
Aminulhoque1 <aminulkupa50@gmail.com>
2022-11-07 11:04:45 -0800

    four commit

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -35,7 +35,42 @@ const Register = () => {
     return (
         <div className='mt-5'>
 
-            
+            <Card className='container p-5' style={{ width: '30rem' }}>
+
+
+                <ListGroup>
+                    
+                        <h2 className='text-black fw-bold p-3'>Please Register Now</h2>
+                   
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" name='name' placeholder="Enter Your Name" />
+
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Photo Url</Form.Label>
+                            <Form.Control type="text" name='url' placeholder="Enter Your url" />
+
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" name='email' placeholder="Enter email" required/>
+
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name='password' placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Register
+                        </Button>
+                        <p>You have a no Account..<Link to='/login' className='text-orange-600 text-decoration-none'>Login</Link></p>
+                    </Form>
+                </ListGroup>
+            </Card>
 
         </div>
     );
