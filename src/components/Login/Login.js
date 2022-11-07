05608dcaf36186418027fe77840ab361ce4c7a05
Aminulhoque1 <aminulkupa50@gmail.com>
2022-11-07 11:05:57 -0800

    commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -43,7 +43,37 @@ const Login = () => {
     return (
         <div className='mt-5'>
 
-           
+            <Card className='container p-5' style={{ width: '30rem' }}>
+
+
+                <ListGroup>
+
+                    <h2 className='text-black fw-bold p-5'>Please Login Now</h2>
+
+                    <Form onSubmit={handleLogin}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" name='email' placeholder="Enter email" required />
+
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name='password' placeholder="Password" />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                        <p>You have a no Account..<Link to='/register' className='text-orange-600 text-decoration-none'>register</Link></p>
+                    </Form>
+                    <div className='mt-5 '>
+                        <Button variant="warning" onClick={handleGoogle}> <FaGoogle></FaGoogle> <p>Sign In With Google</p></Button>
+                    </div>
+                </ListGroup>
+
+            </Card>
+
         </div>
     );
 };
